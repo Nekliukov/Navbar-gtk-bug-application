ebeb9bb2fe8a33e1e07a9b1ce98361fa3249532c
r_nekliukov <r.nekliukov@softteco.com>
2019-09-12 18:34:24 +0300

    Upload project

---
+++ Warmup/Warmup/NewPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Warmup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPage : ContentPage
+    {
+        public NewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
